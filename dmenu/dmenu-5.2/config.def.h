62f38e3334d2bed0dc9dc7db6b2eb4105a7f870f
clementef <clementeferrerspam@tutanota.com>
2023-03-30 00:10:33 -0700

    pdfmenu, vimmenu

--- dmenu/dmenu-5.2/config.def.h
+++ dmenu/dmenu-5.2/config.def.h
@@ -2,12 +2,12 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int max_width = 900;                    /* maximum width when centered */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 300;                    /* minimum width when centered */
-static int max_width = 600;                    /* maximum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Fira Code:size=18"
+	"Fira Code:size=15"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][3] = {
