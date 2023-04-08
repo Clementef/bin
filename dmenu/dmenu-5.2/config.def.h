41521e0ef6790efe93d0d3ea07659c9a266ef13e
clementef <clementeferrerspam@tutanota.com>
2023-04-07 23:27:44 -0700

    dwmblocks

--- dmenu/dmenu-5.2/config.def.h
+++ dmenu/dmenu-5.2/config.def.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int max_width = 900;                    /* maximum width when centered */
+static int max_width = 1000;                    /* maximum width when centered */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 300;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -3,6 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "s_dwmblocks_battery", 1, 0}, 
 	{"", "s_dwmblocks_network", 1, 0},
+	{"", "s_dwmblocks_cpu", 1, 0},
 	{"", "s_dwmblocks_ram", 1, 0},
 	{"", "s_dwmblocks_date", 1, 0},
 };
